a982bd7bf90c00c45bcab4dfa2ea645786630b6a
jwiner <jwiner01@gmail.com>
2022-01-25 11:59:39 +0000

    Exercise for S1.3

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height =100.0 #meters
+
+for i in range(10):
+    height = height * 0.6
+    print(i+1, round(height, 4))
+    
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,32 @@
 # mortgage.py
-#
-# Exercise 1.7
+
+def principal_paid(principal, payment):
+    if payment > principal:
+        return 0, principal
+    else:
+        return principal - payment, payment 
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+over_payment = 1000.0
+over_payment_month_start = 5 * 12 + 1
+over_payment_month_end = over_payment_month_start + 48 - 1
+month = 1
+total_paid = 0.0
+
+while principal > 0:
+    principal = principal * (1+rate/12) 
+    
+    if month >=over_payment_month_start and month <= over_payment_month_end:
+        principal, paid = principal_paid(principal, payment + over_payment)
+    else:
+        principal, paid = principal_paid(principal, payment)
+    
+    total_paid = total_paid + paid
+    print(month, round(total_paid,2), round(principal,2))
+    month = month + 1
+
+
+print('Total paid', round(total_paid,2))
+print('Months', month - 1)
