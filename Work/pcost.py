1f4cbf21f119336fda0d565521c9a249d8b33829
jwiner <jwiner01@gmail.com>
2022-01-27 15:48:02 +0000

    Up to & incl Ex 2.4

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,12 +1,31 @@
 # pcost.py
 #
-# Exercise 1.27
+# Exercise .30
+# def portfolio_cost(filename):   
+#     total_cost = 0
+#     with open(filename, 'rt') as f:
+#         next(f)
+#         for line in f:
+#             _, numshares, price = tuple(line.split(','))
+#             total_cost += int(numshares) * float(price)
+#     return total_cost
 
-total_cost = 0
-with open('Data\portfolio.csv', 'rt') as f:
-    next(f)
-    for line in f:
-        _, numshares, price = tuple(line.split(','))
-        total_cost += int(numshares) * float(price)
-        
-print(f'Total cost: {total_cost}')
+import sys # ex 1.33
+# Exercise 1.32
+import csv
+def portfolio_cost(filename):
+    f = open(filename)
+    rows = csv.reader(f)
+    headers = next(rows)
+    total_cost = 0
+    for row in rows:
+        total_cost += int(row[1]) * float(row[2])
+    return total_cost
+
+if len(sys.argv)==2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data\portfolio.csv'
+
+cost = portfolio_cost(filename)
+print(f'Total cost: {cost: 10.2f}')
--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,16 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+def read_portfolio(filename):
+    f = open(filename)
+    rows = csv.reader(f)
+    headers = next(rows)
+    portfolio = []
+    for row in rows:
+        holding = row[0], int(row[1]), float(row[2])
+        portfolio.append(holding)
+    return portfolio
+
+portfolio = read_portfolio('Data\portfolio.csv')
+print(portfolio)
